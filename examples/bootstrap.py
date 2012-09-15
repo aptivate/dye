d49d4eb2283f172ca96c1d087fe42c6a0ca4d80f
Hamish Downer <dmishd@gmail.com>
2012-09-15 17:18:39 +0100

    First version of bootstrap script

---
+++ examples/bootstrap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# a script to set up the virtualenv so we can use fabric and tasks
+
+import os, sys, subprocess
+
+import project_settings
+
+django_dir = os.path.join(os.dirname(__file__), '..', project_settings.django_dir)
+
+python26 = os.path.join('/', 'usr', 'bin', 'python2.6')
+python27 = os.path.join('/', 'usr', 'bin', 'python2.7')
+generic_python = os.path.join('/', 'usr', 'bin', 'python')
+paths_to_try = (python26, python27, generic_python, sys.executable)
+chosen_python = None
+for python in paths_to_try:
+    if os.path.exists(python):
+        chosen_python = python
+if chosen_python is None:
+    raise Exception("Failed to find a valid Python executable " +
+            "in any of these locations: %s" % paths_to_try)
+
+subprocess.call([chosen_python, 'manage.py', 'update_ve'])
