6d4e84bf20e7e90f5bd05f6dfbae289ba2908bd7
Chris Wilson <chris+git@qwirx.com>
2012-11-29 22:27:45 +0000

    There's no README.rst, but there is a README.md.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url='http://pypi.python.org/pypi/Dye/',
     license='LICENSE.txt',
     description='A set of functions to improve deploy scripts',
-    long_description=open('README.rst').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         "fabric >= 0.9",
     ],
