9f924e1f728cc3c42fff91d76398c13e8bb4c33b
Hamish Downer <hamish@aptivate.org>
2013-07-02 13:03:43 +0100

    Refactor redundant code in link_local_settings()

--- dye/tasklib.py
+++ dye/tasklib.py
@@ -375,37 +375,33 @@ def create_private_settings():
 
 def link_local_settings(environment):
     """ link local_settings.py.environment as local_settings.py """
+    if not env['quiet']:
+        print "### creating link to local_settings.py"
 
     # check that settings imports local_settings, as it always should,
     # and if we forget to add that to our project, it could cause mysterious
     # failures
-    settings_file = path.join(env['django_settings_dir'], 'settings.py')
-    if not(path.isfile(settings_file)):
+    settings_file_path = path.join(env['django_settings_dir'], 'settings.py')
+    if not(path.isfile(settings_file_path)):
         raise InvalidProjectError("Fatal error: settings.py doesn't seem to exist")
-    with open(settings_file) as settings_file:
-        matching_lines = [line for line in settings_file
-            if line.find('local_settings')]
+    with open(settings_file_path) as settings_file:
+        matching_lines = [line for line in settings_file if 'local_settings' in line]
     if not matching_lines:
-        raise InvalidProjectError("Fatal error: settings.py doesn't seem to import " +
-            "local_settings.*: %s" % settings_file)
-
-    # die if the correct local settings does not exist
-    if not env['quiet']:
-        print "### creating link to local_settings.py"
-    local_settings_env_path = path.join(env['django_settings_dir'],
-            'local_settings.py.' + environment)
-    if not path.exists(local_settings_env_path):
-        raise InvalidProjectError("Could not find file to link to: %s" % local_settings_env_path)
-
-    files_to_remove = ('local_settings.py', 'local_settings.pyc')
-    for file in files_to_remove:
-        full_path = path.join(env['django_settings_dir'], file)
-        if path.exists(full_path):
-            os.remove(full_path)
+        raise InvalidProjectError("Fatal error: settings.py doesn't seem to import "
+            "local_settings.*: %s" % settings_file_path)
 
     source = path.join(env['django_settings_dir'], 'local_settings.py.%s' %
         environment)
     target = path.join(env['django_settings_dir'], 'local_settings.py')
+
+    # die if the correct local settings does not exist
+    if not path.exists(source):
+        raise InvalidProjectError("Could not find file to link to: %s" % source)
+
+    # remove any old versions, plus the pyc copy
+    for old_file in (target, target + 'c'):
+        if path.exists(old_file):
+            os.remove(old_file)
 
     if os.name == 'posix':
         os.symlink('local_settings.py.%s' % environment, target)
@@ -413,10 +409,8 @@ def link_local_settings(environment):
         try:
             import win32file
         except ImportError:
-            raise Exception("It looks like the PyWin32 extensions are not " +
-                "installed")
-        if path.exists(target):
-            os.unlink(target)
+            raise Exception(
+                "It looks like the PyWin32 extensions are not installed")
         try:
             win32file.CreateSymbolicLink(target, source)
         except NotImplementedError:
