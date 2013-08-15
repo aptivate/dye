4d91ad2d3e017c3d7bf9a71879c74b97aada48fe
Hamish Downer <hamish@aptivate.org>
2013-08-15 11:48:09 +0100

    fab: do some work on a copy of the project to reduce downtime
    NOTE: STILL NEEDS TESTING.  Committing now so I can untangle branches.
    Also still need to handle a few edge cases - see TODO: comments in
    fablib.
    
    So the broad order used to be:
    
    - do a little work, including making a copy for rollback inc. db dump
    - take down the site
    - do VCS update virtualenv, database migrations etc
    - bring site back up
    
    now it is
    
    - make a copy of the site as "next"
    - in next, do the VCS update and virtual env stuff
    - take down the site
    - move the current site to rollback and do database dump
    - move next to replace the old current site
    - do database migrations etc
    - bring site back up
    
    Which should mean a lot less site downtime when the virtualenv is being
    updated.

--- dye/fablib.py
+++ dye/fablib.py
@@ -25,8 +25,10 @@ def _setup_paths(project_settings):
     env.setdefault('default_branch', {'production': 'master', 'staging': 'master'})
     env.setdefault('server_project_home',
                    path.join(env.server_home, env.project_name))
+    # TODO: change dev -> current
     env.setdefault('vcs_root_dir', path.join(env.server_project_home, 'dev'))
     env.setdefault('prev_root', path.join(env.server_project_home, 'previous'))
+    env.setdefault('next_dir', path.join(env.server_project_home, 'next'))
     env.setdefault('dump_dir', path.join(env.server_project_home, 'dbdumps'))
     env.setdefault('deploy_dir', path.join(env.vcs_root_dir, 'deploy'))
     env.setdefault('settings', '%(project_name)s.settings' % env)
@@ -145,8 +147,19 @@ def deploy(revision=None, keep=None):
 
     _create_dir_if_not_exists(env.server_project_home)
 
-    if files.exists(env.vcs_root_dir):
-        create_copy_for_rollback(keep)
+    # TODO: check if our live site is in <sitename>/dev/ - if so
+    # move it to <sitename>/current/ and make a link called dev/ to
+    # the current/ directory
+    # TODO: if dev/ is found to be a link, ask the user if the apache config
+    # has been updated to point at current/ - and if so then delete dev/
+    # _migrate_from_dev_to_current()
+    create_copy_for_next()
+    checkout_or_update(in_next=True, revision=revision)
+    # remove any old pyc files - essential if the .py file has been removed
+    if env.project_type == "django":
+        rm_pyc_files(path.join(env.next_dir, env.relative_django_dir))
+    # create the deploy virtualenv if we use it
+    create_deploy_virtualenv(in_next=True)
 
     # we only have to disable this site after creating the rollback copy
     # (do this so that apache carries on serving other sites on this server
@@ -155,20 +168,12 @@ def deploy(revision=None, keep=None):
     link_webserver_conf(maintenance=True)
     with settings(warn_only=True):
         webserver_cmd('reload')
-    checkout_or_update(revision)
-    # remove any old pyc files - essential if the .py file has been removed
-    if env.project_type == "django":
-        rm_pyc_files()
+    next_to_current_to_rollback()
 
-    # create the deploy virtualenv if we use it
-    create_deploy_virtualenv()
     # Use tasks.py deploy:env to actually do the deployment, including
     # creating the virtualenv if it thinks it necessary, ignoring
     # env.use_virtualenv as tasks.py knows nothing about it.
     _tasks('deploy:' + env.environment)
-
-    if env.environment == 'production':
-        setup_db_dumps()
 
     # bring this vhost back in, reload the webserver and touch the WSGI
     # handler (which reloads the wsgi app)
@@ -176,6 +181,11 @@ def deploy(revision=None, keep=None):
     webserver_cmd('reload')
     downtime_end = datetime.now()
     touch_wsgi()
+
+    delete_old_rollback_versions(keep)
+    if env.environment == 'production':
+        setup_db_dumps()
+
     _report_downtime(downtime_start, downtime_end)
 
 
@@ -208,14 +218,59 @@ def set_up_celery_daemon():
         sudo_or_run('/etc/init.d/%s restart' % command)
 
 
-def create_copy_for_rollback(keep):
-    """Copy the current version out of the way so we can rollback to it if required."""
-    require('django_settings_dir', provided_by=env.valid_envs)
+def create_copy_for_next():
+    """Copy the current version to "next" so that we can do stuff like
+    the VCS update and virtualenv update without taking the site offline"""
+    # TODO: check if next directory already exists
+    # if it does maybe there was an aborted deploy, or maybe someone else is
+    # deploying.  Either way, stop and ask the user what to do.
+    if files.exists(env.next_dir):
+        utils.warn('The "next" directory already exists.  Maybe a previous deploy'
+                   'failed, or maybe another deploy is in progress.')
+        continue_anyway = prompt('Would you like to continue anyway '
+                                 '(and delete the current next dir)? [no/yes]',
+                default='no', validate='^no|yes$')
+        if continue_anyway.lower() != 'yes':
+            utils.abort("Aborting deploy - try again when you're certain what to do.")
+        sudo_or_run('rm -rf %s' % env.next_dir)
+
+    # if this is the initial deploy, the vcs_root_dir won't exist yet. In that
+    # case, don't create it (otherwise the checkout code will get confused).
+    if files.exists(env.vcs_root_dir):
+        # create directory for the next version
+        _create_dir_if_not_exists(env.next_dir)
+        # cp -a
+        sudo_or_run('cp -a %s/* %s' % (env.vcs_root_dir, env.next_dir))
+
+
+def next_to_current_to_rollback():
+    """Move the current version to the previous directory (so we can roll back
+    to it, move the next version to the current version (so it will be used) and
+    do a db dump in the rollback directory."""
+    # create directory for it
+    # if this is the initial deploy, the vcs_root_dir won't exist yet.  In that
+    # case just skip the rollback version.
+    if files.exists(env.vcs_root_dir):
+        prev_dir = path.join(env.prev_root, time.strftime("%Y-%m-%d_%H-%M-%S"))
+        sudo_or_run('mv %s %s' % (env.vcs_root_dir, prev_dir))
+        _dump_db_in_previous_directory(prev_dir)
+    sudo_or_run('mv %s %s' % (env.next_dir, env.vcs_root_dir))
+
+
+def create_copy_for_rollback():
+    """Move the current version to the previous directory (so we can roll back
+    to it, move the next version to the current version (so it will be used) and
+    do a db dump in the rollback directory."""
     # create directory for it
     prev_dir = path.join(env.prev_root, time.strftime("%Y-%m-%d_%H-%M-%S"))
     _create_dir_if_not_exists(prev_dir)
     # cp -a
-    sudo_or_run('cp -a %s %s' % (env.vcs_root_dir, prev_dir))
+    sudo_or_run('cp %s %s' % (env.vcs_root_dir, prev_dir))
+    _dump_db_in_previous_directory(prev_dir)
+
+
+def _dump_db_in_previous_directory(prev_dir):
+    require('django_settings_dir', provided_by=env.valid_envs)
     if (env.project_type == 'django' and
             files.exists(path.join(env.django_settings_dir, 'local_settings.py'))):
         # dump database (provided local_settings has been set up properly)
@@ -223,11 +278,9 @@ def create_copy_for_rollback(keep):
             # just in case there is some other reason why the dump fails
             with settings(warn_only=True):
                 _tasks('dump_db')
-    if keep is None or int(keep) > 0:
-        delete_old_versions(keep)
 
 
-def delete_old_versions(keep=None):
+def delete_old_rollback_versions(keep=None):
     """Delete old rollback directories, keeping the last "keep" (default 5)"."""
     require('prev_root', provided_by=env.valid_envs)
     # the -1 argument ensures one directory per line
@@ -237,6 +290,10 @@ def delete_old_versions(keep=None):
             keep = env.versions_to_keep
         else:
             keep = 5
+    else:
+        keep = int(keep)
+    if keep == 0:
+        return
     versions_to_keep = -1 * int(keep)
     prev_versions_to_delete = prev_versions[:versions_to_keep]
     for version_to_delete in prev_versions_to_delete:
@@ -277,21 +334,20 @@ def rollback(version='last', migrate=False, restore_db=False):
         # list directories in env.prev_root, use last one
         version = run('ls ' + env.prev_root).split('\n')[-1]
     # check version specified exists
-    rollback_dir_base = path.join(env.prev_root, version)
-    rollback_dir = path.join(rollback_dir_base, 'dev')
+    rollback_dir = path.join(env.prev_root, version)
     if not files.exists(rollback_dir):
         utils.abort("Cannot rollback to version %s, it does not exist, use list_previous to see versions available" % version)
 
     webserver_cmd("stop")
     # first copy this version out of the way
-    create_copy_for_rollback(-1)
+    create_copy_for_rollback()
     if migrate:
         # run the south migrations back to the old version
         # but how to work out what the old version is??
         pass
     if restore_db:
         # feed the dump file into mysql command
-        with cd(rollback_dir_base):
+        with cd(rollback_dir):
             _tasks('load_dbdump')
     # delete everything - don't want stray files left over
     sudo_or_run('rm -rf %s' % env.vcs_root_dir)
@@ -398,7 +454,7 @@ def check_for_local_changes():
             _check_git_branch()
 
 
-def checkout_or_update(revision=None):
+def checkout_or_update(in_next=False, revision=None):
     """ checkout or update the project from version control.
 
     This command works with svn, git and cvs repositories.
@@ -411,28 +467,32 @@ def checkout_or_update(revision=None):
         'svn': _checkout_or_update_svn,
         'git': _checkout_or_update_git,
     }
+    if in_next:
+        vcs_root_dir = env.next_dir
+    else:
+        vcs_root_dir = env.vcs_root_dir
     if env.repo_type.lower() in checkout_fn:
-        checkout_fn[env.repo_type](revision)
+        checkout_fn[env.repo_type](vcs_root_dir, revision)
     else:
         utils.abort('Unsupported VCS: %s' % env.repo_type.lower())
 
 
-def _checkout_or_update_svn(revision=None):
+def _checkout_or_update_svn(vcs_root_dir, revision=None):
     # function to ask for svnuser and svnpass
     _get_svn_user_and_pass()
     # if the .svn directory exists, do an update, otherwise do
     # a checkout
     cmd = 'svn %s --non-interactive --no-auth-cache --username %s --password %s'
-    if files.exists(path.join(env.vcs_root_dir, ".svn")):
+    if files.exists(path.join(vcs_root_dir, ".svn")):
         cmd = cmd % ('update', env.svnuser, env.svnpass)
         if revision:
             cmd += " --revision " + revision
-        with cd(env.vcs_root_dir):
+        with cd(vcs_root_dir):
             with hide('running'):
                 sudo_or_run(cmd)
     else:
         cmd = cmd + " %s %s"
-        cmd = cmd % ('checkout', env.svnuser, env.svnpass, env.repository, env.vcs_root_dir)
+        cmd = cmd % ('checkout', env.svnuser, env.svnpass, env.repository, vcs_root_dir)
         if revision:
             cmd += "@" + revision
         with cd(env.server_project_home):
@@ -440,11 +500,11 @@ def _checkout_or_update_svn(revision=None):
                 sudo_or_run(cmd)
 
 
-def _checkout_or_update_git(revision=None):
+def _checkout_or_update_git(vcs_root_dir, revision=None):
     # if the .git directory exists, do an update, otherwise do
     # a clone
-    if files.exists(path.join(env.vcs_root_dir, ".git")):
-        with cd(env.vcs_root_dir):
+    if files.exists(path.join(vcs_root_dir, ".git")):
+        with cd(vcs_root_dir):
             sudo_or_run('git remote rm origin')
             sudo_or_run('git remote add origin %s' % env.repository)
             # fetch now, merge later (if on branch)
@@ -453,7 +513,7 @@ def _checkout_or_update_git(revision=None):
         if revision is None:
             revision = env.revision
 
-        with cd(env.vcs_root_dir):
+        with cd(vcs_root_dir):
             stash_result = sudo_or_run('git stash')
             sudo_or_run('git checkout %s' % revision)
             # check if revision is a branch, and do a merge if it is
@@ -469,16 +529,16 @@ def _checkout_or_update_git(revision=None):
         with cd(env.server_project_home):
             default_branch = env.default_branch.get(env.environment, 'master')
             sudo_or_run('git clone -b %s %s %s' %
-                    (default_branch, env.repository, env.vcs_root_dir))
+                    (default_branch, env.repository, vcs_root_dir))
 
-    if files.exists(path.join(env.vcs_root_dir, ".gitmodules")):
-        with cd(env.vcs_root_dir):
+    if files.exists(path.join(vcs_root_dir, ".gitmodules")):
+        with cd(vcs_root_dir):
             sudo_or_run('git submodule update --init')
 
 
-def _checkout_or_update_cvs(revision):
-    if files.exists(env.vcs_root_dir):
-        with cd(env.vcs_root_dir):
+def _checkout_or_update_cvs(vcs_root_dir, revision=None):
+    if files.exists(vcs_root_dir):
+        with cd(vcs_root_dir):
             sudo_or_run('CVS_RSH="ssh" cvs update -d -P')
     else:
         if 'cvs_user' in env:
@@ -491,7 +551,7 @@ def _checkout_or_update_cvs(revision):
                                              user_spec,
                                              env.repository,
                                              env.repo_path)
-            command_options = '-d %s' % env.vcs_root_dir
+            command_options = '-d %s' % vcs_root_dir
 
             if revision is not None:
                 command_options += ' -r ' + revision
@@ -508,10 +568,14 @@ def sudo_or_run(command):
         return run(command)
 
 
-def create_deploy_virtualenv():
+def create_deploy_virtualenv(in_next=False):
     """ if using new style dye stuff, create the virtualenv to hold dye """
     require('deploy_dir', provided_by=env.valid_envs)
-    bootstrap_path = path.join(env.deploy_dir, 'bootstrap.py')
+    if in_next:
+        # TODO: use relative_deploy_dir
+        bootstrap_path = path.join(env.next_dir, 'deploy', 'bootstrap.py')
+    else:
+        bootstrap_path = path.join(env.deploy_dir, 'bootstrap.py')
     sudo_or_run('%s %s --quiet' % (_get_python(), bootstrap_path))
 
 
@@ -577,11 +641,13 @@ def touch_wsgi():
     sudo_or_run('touch ' + path.join(wsgi_dir, 'wsgi_handler.py'))
 
 
-def rm_pyc_files():
+def rm_pyc_files(py_dir=None):
     """Remove all the old pyc files to prevent stale files being used"""
     require('django_dir', provided_by=env.valid_envs)
+    if py_dir is None:
+        py_dir = env.django_dir
     with settings(warn_only=True):
-        with cd(env.django_dir):
+        with cd(py_dir):
             sudo_or_run('find . -name \*.pyc | xargs rm')
 
 
