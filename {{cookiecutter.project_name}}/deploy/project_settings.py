fe04ebaabbd91078cd5a1f48edeb6c05fb8bbb70
tomd <tomd+git@aptivate.org>
2014-03-27 17:12:06 +0000

    Use standard prefix-stage.fen. name for staging

--- {{cookiecutter.project_name}}/deploy/project_settings.py
+++ {{cookiecutter.project_name}}/deploy/project_settings.py
@@ -74,7 +74,7 @@ test_cmd = ' manage.py test -v0 ' + ' '.join(django_apps)
 # production server - if commented out then the production task will abort
 host_list = {
     'production':   ['lin-' + project_name + '.aptivate.org:48001'],
-    'staging':      ['fen-vz-' + project_name + '.fen.aptivate.org'],
+    'staging':      ['fen-vz-' + project_name + '-stage.fen.aptivate.org'],
     'staging_test': ['fen-vz-' + project_name + '.fen.aptivate.org'],
     'dev_server':   ['fen-vz-' + project_name + '-dev.fen.aptivate.org'],
 }
