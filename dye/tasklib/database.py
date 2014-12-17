aptivate/dye

commit 53bea9d24c9535f9ac5384ee070257ea3dc4ab12

Fix test database name on SQLite, and test_grants() method.
Allows update_db to work properly on SQLite databases.

dye/tasklib/database.py
@@ -55,6 +55,11 @@ class SqliteManager(DBManager):
         else:
             self.file_path = path.abspath(path.join(root_dir, name))
 
+    def get_test_database(self):
+        test_db_filename = path.join(path.dirname(self.file_path),
+            'test_%s' % path.basename(self.file_path))
+        return SqliteManager(name=test_db_filename, root_dir=None)
+
     def drop_db(self):
         if path.exists(self.file_path):
             os.remove(self.file_path)
@@ -81,6 +86,10 @@ class SqliteManager(DBManager):
             return len(list(result.fetchall())) != 0
         finally:
             conn.close()
+
+    # There is no security on SQLite databases
+    def test_grants(self):
+        return True
 
     # this is used directly for the test database
     def grant_all_privileges_for_database(self):
@@ -121,6 +130,11 @@ class MySQLManager(DBManager):
         # user
         self.user_db_conn = None
         self.root_db_conn = None
+
+    def get_test_database(self):
+        return MySQLManager(name='test_%s' % self.name, user=self.user,
+            password=self.password, port=self.port, host=self.host,
+            root_password=sef.root_password, grant_enabled=self.grant_enabled)
 
     def get_root_password(self):
         """This can be overridden (by monkeypatching) if required."""

dye/tasklib/django.py
@@ -118,8 +118,7 @@ def _create_db_objects(database='default'):
     # and create the objects that hold the db details
     env['db'] = get_db_manager(**db_details)
     # and the test db object
-    db_details['name'] = 'test_' + db_details['name']
-    env['test_db'] = get_db_manager(**db_details)
+    env['test_db'] = env['db'].get_test_database()
 
 
 def clean_db(database='default'):

commit b3cbac76af81ae1ba26661b6332150f245aa93b3

Fix bug sef.root_password
Introduced in 53bea9d24c9535f9ac5384ee070257ea3dc4ab12

dye/tasklib/database.py
@@ -177,7 +177,7 @@ class MySQLManager(DBManager):
     def get_test_database(self):
         return MySQLManager(name='test_%s' % self.name, user=self.user,
             password=self.password, port=self.port, host=self.host,
-            root_password=sef.root_password, grant_enabled=self.grant_enabled)
+            root_password=self.root_password, grant_enabled=self.grant_enabled)
 
     def get_root_password(self):
         """This can be overridden (by monkeypatching) if required."""